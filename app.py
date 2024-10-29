dd09dfd449f2441ff7b6a26d699b17112c1d245b
JaiDhiman <jaidhiman2000@gmail.com>
2024-10-28 18:59:55 -0700

    restful api

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask, request
+import db
 
 app = Flask(__name__)
 
@@ -6,3 +7,29 @@ app = Flask(__name__)
 @app.route('/')
 def hello():
     return 'Hello, World!'
+
+@app.route("/photos.json")
+def index():
+    return db.photos_all()
+
+@app.route("/photos/<id>.json")
+def show(id):
+    return db.photos_find_by_id(id)
+
+@app.route("/photos.json", methods=["POST"])
+def create():
+    name = request.form.get("name")
+    width = request.form.get("width")
+    height = request.form.get("height")
+    return db.photos_create(name, width, height)
+
+@app.route("/photos/<id>.json", methods=["PATCH"])
+def update(id):
+    name = request.form.get("name")
+    width = request.form.get("width")
+    height = request.form.get("height")
+    return db.photos_update_by_id(id, name, width, height)
+
+@app.route("/photos/<id>.json", methods=["DELETE"])
+def destroy(id):
+    return db.photos_destroy_by_id(id)
--- db.py
+++ db.py
@@ -47,3 +47,61 @@ def initial_setup():
 
 if __name__ == "__main__":
     initial_setup()
+
+def photos_all():
+    conn = connect_to_db()
+    rows = conn.execute(
+        """
+        SELECT * FROM photos
+        """
+    ).fetchall()
+    return [dict(row) for row in rows]   
+
+def photos_find_by_id(id):
+    conn = connect_to_db()
+    row = conn.execute(
+        """
+        SELECT * FROM photos
+        WHERE id = ?
+        """,
+        (id,),
+    ).fetchone()
+    return dict(row)
+
+def photos_create(name, width, height):
+    conn = connect_to_db()
+    row = conn.execute(
+        """
+        INSERT INTO photos (name, width, height)
+        VALUES (?, ?, ?)
+        RETURNING *
+        """,
+        (name, width, height),
+    ).fetchone()
+    conn.commit()
+    return dict(row)
+
+def photos_update_by_id(id, name, width, height):
+    conn = connect_to_db()
+    row = conn.execute(
+        """
+        UPDATE photos SET name = ?, width = ?, height = ?
+        WHERE id = ?
+        RETURNING *
+        """,
+        (name, width, height, id),
+    ).fetchone()
+    conn.commit()
+    return dict(row)
+
+def photos_destroy_by_id(id):
+    conn = connect_to_db()
+    row = conn.execute(
+        """
+        DELETE from photos
+        WHERE id = ?
+        """,
+        (id,),
+    )
+    conn.commit()
+    return {"message": "Photo destroyed successfully"}
